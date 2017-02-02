5fdbcd88e249539e9f14cd1e53ae9af8066208ec
Akram <akramewu@gmail.com>
2017-02-02 22:47:10 +0100

    Prime Number

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int userInput = Convert.ToInt16(Console.ReadLine()); //Take a user input
+            int i;
+            for (i = 2; i <=userInput; i++) //for Prime number i value start from 2
+            {
+                if(userInput % i == 0)
+                {
+                    break;
+                }
+            }
+
+            if(i == userInput)
+            {
+                Console.WriteLine("Number is Prime");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Number is not Prime");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
